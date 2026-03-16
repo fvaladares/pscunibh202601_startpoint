d87e3de2a6f2d4b96b4215b7f1da80eef2763b48
Fabricio Valadares <fabricio.valadares@gmail.com>
2026-03-16 09:30:42 -0300

    First commit

---
+++ src/main/java/br/unibh/psc202601/Main.java
@@ -0,0 +1,14 @@
+package br.unibh.psc202601;
+
+// Importação da biblioteca de entrada padrão
+import java.util.Scanner;
+
+// O nome da classe (que está após o public class), deve ser igual ao nome do arquivo!
+// Nome de classe sempre começa com letra maiúscula
+public class Main {
+    public static void main(String[] args) {
+        // Criação do objeto responsável pela leitura dos dados.
+        // Nome de variável deve sempre começar com letra minúscula!!
+        Scanner entrada = new Scanner(System.in);
+    }
+}
